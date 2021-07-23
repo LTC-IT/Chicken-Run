a88714eb661a1bbfe2648d4d712083c986c3458d
LTC-IT <ryan.cather@ed.act.edu.au>
2021-07-23 21:14:02 +1000

    init

---
+++ Assets/Scripts/randomSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomSpawn : MonoBehaviour {
+
+    public GameObject[] respawns;
+    public GameObject respawnPrefab;
+
+    void Start () {
+        respawns = GameObject.FindGameObjectsWithTag ("spawnPoint");
+
+        for (int i = 0; i < 20; i++) {
+            int randomSpawnPoint = Random.Range (0, 5);
+            Debug.Log ("Spawn Point ID:" + randomSpawnPoint);
+            Instantiate (respawnPrefab, respawns[randomSpawnPoint].transform.position, Quaternion.identity);
+        }
+    }
+
+}
---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+	private Text textField;
+	private int currentTime;
+
+	void Awake () {
+		textField = GameObject.Find ("Canvas/timer").GetComponent<Text> ();
+		UpdateTimerText ();
+		StartCoroutine (Tick ());
+	}
+
+	// Update the GUI
+	void UpdateTimerText () {
+		textField.text = "Time: " + currentTime.ToString ();
+	}
+
+	IEnumerator Tick () {
+		while (true) {
+			Debug.Log ("Time: " + currentTime);
+			yield return new WaitForSeconds (1);
+			currentTime = currentTime + 1;
+			UpdateTimerText ();
+		}
+	}
+}
